a013b1d67b41a77e5d8da740a1c9501a7a9a15c2
Aleksandr6669 <50256477+Aleksandr6669@users.noreply.github.com>
2026-04-16 21:31:56 +0000

    new file:   .github/workflows/ios_watos_build.yml new file:   ios/WatchApp Extension/NotificationController.swift new file:   ios/WatchApp/ContentView.swift new file:   ios/WatchApp/MyWatchApp.swift

---
+++ ios/WatchApp Extension/NotificationController.swift
@@ -0,0 +1,8 @@
+import WatchKit
+import Foundation
+
+class NotificationController: WKUserNotificationHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ ios/WatchApp/ContentView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Привет, мир!")
+            .font(.title2)
+            .padding()
+    }
+}
