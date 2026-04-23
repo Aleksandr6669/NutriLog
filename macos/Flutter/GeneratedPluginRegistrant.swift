5fa575bf2a8086628c3ada261d8b2f2910cc34aa
Aleksandr6669 <50256477+Aleksandr6669@users.noreply.github.com>
2026-04-23 10:36:40 +0000

    modified:   lib/screens/profile/connections_notifications_screen.dart modified:   lib/services/app_notification_service.dart modified:   macos/Flutter/GeneratedPluginRegistrant.swift modified:   pubspec.lock modified:   pubspec.yaml

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import device_info_plus
 import file_selector_macos
 import flutter_local_notifications
@@ -14,6 +15,7 @@ import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
