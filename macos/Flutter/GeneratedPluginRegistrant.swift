3b784354f8a5af9dee6997cc96d8b53a7e7644f0
Aleksandr6669 <Gabreil666@gmail.com>
2026-04-29 02:36:49 +0300

    modified:   lib/services/app_notification_service.dart modified:   linux/flutter/generated_plugin_registrant.cc modified:   linux/flutter/generated_plugins.cmake modified:   macos/Flutter/GeneratedPluginRegistrant.swift modified:   pubspec.lock modified:   pubspec.yaml

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,18 +6,18 @@ import FlutterMacOS
 import Foundation
 
 import app_settings
+import awesome_notifications
 import device_info_plus
 import file_selector_macos
-import flutter_local_notifications
 import flutter_timezone
 import package_info_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
