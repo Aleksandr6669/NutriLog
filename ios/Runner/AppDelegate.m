4980d175cde022ed4451553d4cfcaeae8de18af8
Aleksandr6669 <Gabreil666@gmail.com>
2026-05-25 02:22:21 +0300

    fix: initialize Flutter engine correctly in AppDelegate to resolve launch issues

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -15,7 +15,7 @@
   // Перезагружаем все виджеты при уходе в фон.
   // К этому моменту Flutter записал данные в UserDefaults через App Group.
   if (@available(iOS 16.0, *)) {
-    [WidgetCenter.shared reloadAllTimelines];
+    [[WidgetCenter shared] reloadAllTimelines];
   }
   [super applicationDidEnterBackground:application];
 }
